77ed12f419d939d35c34ee0c97b117aae0ebe53d
Ryo Kurosu <knowrop1208@gmail.com>
2019-09-11 00:35:27 +0900

    fix

--- cron.sh
+++ cron.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
+
+killall python
+killall chromedriver
+killall chrome
+
 export DISPLAY=:0
 PROG_DIR=/home/root/app
 source $PROG_DIR/env/bin/activate
 
-python $PROG_DIR/cron.py
+python $PROG_DIR/cron.py &
