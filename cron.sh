34d31a2fa126ca8ca6f4e76446ea7b20f6d78bc6
Ryo Kurosu <knowrop1208@gmail.com>
2019-09-14 00:21:30 +0900

    local

--- cron.sh
+++ cron.sh
@@ -4,8 +4,8 @@ killall python
 killall chromedriver
 killall chrome
 
-export DISPLAY=:0
-PROG_DIR=/home/root/app
+# export DISPLAY=:0
+PROG_DIR=`dirname $0`
 
 cd $PROG_DIR
 git stash
@@ -14,4 +14,4 @@ git fetch
 git reset --hard origin/master
 
 source $PROG_DIR/env/bin/activate
-python $PROG_DIR/cron.py > $PROG_DIR/logs/`date +%Y%m%d_%H%M%S`.log
+nohup python3 $PROG_DIR/cron.py > $PROG_DIR/logs/`date +%Y%m%d_%H%M%S`.log &
